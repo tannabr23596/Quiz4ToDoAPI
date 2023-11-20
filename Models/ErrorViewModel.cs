b1b4752232eebc241fc12c2096975518440dac8b
tannabr23596 <tabrah13@my.centennialcollege.ca>
2023-11-20 14:37:32 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TodoApiQuiz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
